0ba9e2f1b0250a28476c5ff054e937f7de539df0
M. Zakiyuddin Munziri <belummikir@gmail.com>
2024-02-10 20:11:15 +0800

    feat: change font to Plus_Jakarta_Sans

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Plus_Jakarta_Sans } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const plusJakartaSans = Plus_Jakarta_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
 	title: "Sini Cerita",
@@ -16,7 +16,7 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
-			<body className={inter.className}>{children}</body>
+			<body className={plusJakartaSans.className}>{children}</body>
 		</html>
 	);
 }
