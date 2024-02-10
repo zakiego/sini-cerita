79232fc277f730d522ab75bf425f348827b26845
M. Zakiyuddin Munziri <belummikir@gmail.com>
2024-02-11 05:17:03 +0800

    feat: use force-dynamic and timestamp in server

--- src/app/(home)/@components/feeds.tsx
+++ src/app/(home)/@components/feeds.tsx
@@ -16,6 +16,7 @@ interface Props {
   stories: {
     content: string;
     timestamp: string;
+    readableTimestamp: string;
   }[];
 }
 
@@ -118,7 +119,7 @@ export const Feeds = ({ stories }: Props) => {
                       className="flex-none py-0.5 text-xs leading-5 text-gray-500"
                       suppressHydrationWarning
                     >
-                      {formatDateDifference(activityItem.timestamp)}
+                      {activityItem.timestamp}
                     </time>
                   </div>
                   {!isContentLong && (
@@ -152,30 +153,3 @@ export const Feeds = ({ stories }: Props) => {
     </>
   );
 };
-
-function formatDateDifference(inputDate: string): string {
-  const date = new Date(inputDate);
-  const currentDate = new Date();
-  const timeDifference = currentDate.getTime() - date.getTime();
-
-  // Function to convert milliseconds to human-readable time
-  const msToTime = (duration: number): string => {
-    const seconds = Math.floor((duration / 1000) % 60);
-    const minutes = Math.floor((duration / (1000 * 60)) % 60);
-    const hours = Math.floor((duration / (1000 * 60 * 60)) % 24);
-    const days = Math.floor(duration / (1000 * 60 * 60 * 24));
-    const weeks = Math.floor(duration / (1000 * 60 * 60 * 24 * 7));
-    const years = Math.floor(duration / (1000 * 60 * 60 * 24 * 365));
-
-    if (years > 0) return `${years} year${years > 1 ? "s" : ""} ago`;
-    if (weeks > 0) return `${weeks} week${weeks > 1 ? "s" : ""} ago`;
-    if (days > 0) return `${days} day${days > 1 ? "s" : ""} ago`;
-    if (hours > 0) return `${hours} hour${hours > 1 ? "s" : ""} ago`;
-    if (minutes > 0) return `${minutes} minute${minutes > 1 ? "s" : ""} ago`;
-    if (seconds > 0) return `${seconds} second${seconds > 1 ? "s" : ""} ago`;
-
-    return "just now";
-  };
-
-  return msToTime(timeDifference);
-}
--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -8,6 +8,8 @@ export const metadata: Metadata = {
   description: "Sini cerita masalahmu, kamu nggak sendiri",
 };
 
+export const dynamic = "force-dynamic";
+
 export default async function Home() {
   const stories = await client.lrange("stories", 0, -1);
   const timestamps = await client.lrange("stories-timestamps", 0, -1);
@@ -15,6 +17,7 @@ export default async function Home() {
   const data = stories.map((content, i) => ({
     content,
     timestamp: timestamps[i],
+    readableTimestamp: formatDateDifference(timestamps[i]),
   }));
 
   const filteredData = data.filter((story) => story.content.trim());
@@ -24,4 +27,31 @@ export default async function Home() {
       <Feeds stories={filteredData} />
     </LayoutPage>
   );
+}
+
+function formatDateDifference(inputDate: string): string {
+  const date = new Date(inputDate);
+  const currentDate = new Date();
+  const timeDifference = currentDate.getTime() - date.getTime();
+
+  // Function to convert milliseconds to human-readable time
+  const msToTime = (duration: number): string => {
+    const seconds = Math.floor((duration / 1000) % 60);
+    const minutes = Math.floor((duration / (1000 * 60)) % 60);
+    const hours = Math.floor((duration / (1000 * 60 * 60)) % 24);
+    const days = Math.floor(duration / (1000 * 60 * 60 * 24));
+    const weeks = Math.floor(duration / (1000 * 60 * 60 * 24 * 7));
+    const years = Math.floor(duration / (1000 * 60 * 60 * 24 * 365));
+
+    if (years > 0) return `${years} year${years > 1 ? "s" : ""} ago`;
+    if (weeks > 0) return `${weeks} week${weeks > 1 ? "s" : ""} ago`;
+    if (days > 0) return `${days} day${days > 1 ? "s" : ""} ago`;
+    if (hours > 0) return `${hours} hour${hours > 1 ? "s" : ""} ago`;
+    if (minutes > 0) return `${minutes} minute${minutes > 1 ? "s" : ""} ago`;
+    if (seconds > 0) return `${seconds} second${seconds > 1 ? "s" : ""} ago`;
+
+    return "just now";
+  };
+
+  return msToTime(timeDifference);
 }
